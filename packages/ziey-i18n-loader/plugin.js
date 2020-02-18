401037ff548bef41b77b9fa3171add0ab7557bb1
zemzheng <zemzheng@gmail.com>
2020-02-18 10:33:43 +0800

    fix(ziey-i18n-loader): use es5

--- packages/ziey-i18n-loader/plugin.js
+++ packages/ziey-i18n-loader/plugin.js
@@ -1,7 +1,7 @@
-import { save } from "./index";
+const { save } = require("./index");
 const pluginName = 'ZieyI18nLoaderPlugin';
 
-export default class ZieyI18nLoaderPlugin {
+module.exports = class ZieyI18nLoaderPlugin {
     apply(compiler) {
         compiler.hooks.afterEmit.tap(pluginName, compilation => {
             save();
